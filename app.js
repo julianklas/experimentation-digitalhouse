e1bfe008e7c0045e1e7cc858f0c105f694c58163
Julián Klas <julian.klas@mercadolibre.com>
2017-06-21 13:52:16 -0300

    hotjar

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ const app = express();
 
 app.set('view engine', 'pug');  
 
+app.get('/', function (req, res) {
+  res.render('baseline',
+    {
+      title: 'A/B testing',
+      analytics: false,
+      optimizely: false,
+      hotjar: false
+    }
+  );
+});
+
+
 app.get('/optimizely', function (req, res) {
   res.render('baseline',
     {
       title: 'A/B testing',
       analytics: false,
-      optimizely: true
+      optimizely: true,
+      hotjar: true
     }
   );
 });
@@ -23,7 +36,8 @@ app.get('/analytics', function (req, res) {
     {
       title: 'A/B testing',
       analytics: true,
-      optimizely: false
+      optimizely: false,
+      hotjar: true
     }
   );
 });
@@ -33,7 +47,8 @@ app.get('/continua', function (req, res) {
     {
       title: 'A/B testing',
       analytics: true,
-      optimizely: true
+      optimizely: true,
+      hotjar: true
     }
   );
 });
@@ -43,7 +58,8 @@ app.get('/abandona', function (req, res) {
     {
       title: 'A/B testing',
       analytics: true,
-      optimizely: true
+      optimizely: true,
+      hotjar: true
     }
   );
 });
